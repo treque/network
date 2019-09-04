42c61c82a6475bac2d1e7decaf65d62d55ab1bb8
m <myriamkiriakos342@gmail.com>
2019-09-04 13:27:35 -0400

    made a class by method we have to implement so that the code is cleaner

---
+++ Server/src/Server/Commands/EXITCommand.java
@@ -0,0 +1,20 @@
+package Server.Commands;
+
+import java.nio.file.Path;
+
+public class EXITCommand extends AbstractCommand 
+{
+
+	EXITCommand(Path currentPath) 
+	{
+		super(currentPath);
+	}
+	
+	@Override
+	public String excecuteCommand(String[] command) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Server/src/Server/Commands/LSCommand.java
@@ -0,0 +1,19 @@
+package Server.Commands;
+
+import java.nio.file.Path;
+
+public class LSCommand extends AbstractCommand {
+
+	LSCommand(Path currentPath) 
+	{
+		super(currentPath);
+	}
+
+	@Override
+	public String excecuteCommand(String[] command) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
